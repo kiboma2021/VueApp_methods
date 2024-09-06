d3d01d23f1a213879dd51369527b4ae7989c1268
kiboma <omayioben@gmail.com>
2024-09-06 08:16:57 +0300

    Dynamic classes

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = Vue.createApp({
         return{
             showData: true,
             books: [
-                { title: 'Shreds of Tenderness', author: 'John Kelly', img: 'assets/images/1.jpeg' },
-                { title: 'Echoes of the Past', author: 'Jane Doe', img: 'assets/images/2.jpeg' },
-                { title: 'The Silent Storm', author: 'Robert Harris', img: 'assets/images/3.jpeg' },
-                { title: 'Whispers in the Dark', author: 'Emily Bronte', img: 'assets/images/4.jpeg' },
-                { title: 'Fading Shadows', author: 'Michael Scott', img: 'assets/images/5.jpeg' },
-                { title: 'Broken Dreams', author: 'Sarah Williams', img: 'assets/images/6.jpeg' },
-                { title: 'Journey to Nowhere', author: 'Tom Jenkins', img: 'assets/images/7.jpeg' },
-                { title: 'Chasing the Wind', author: 'Alice Walker', img: 'assets/images/8.jpeg' },
-                { title: 'Beyond the Horizon', author: 'David Brown', img: 'assets/images/9.jpeg' },
-                { title: 'Fragments of Time', author: 'Laura Hill', img: 'assets/images/10.jpeg' }
+                { title: 'Shreds of Tenderness', author: 'John Kelly', img: 'assets/images/1.jpeg', isFav: true },
+                { title: 'Echoes of the Past', author: 'Jane Doe', img: 'assets/images/2.jpeg', isFav: false },
+                { title: 'The Silent Storm', author: 'Robert Harris', img: 'assets/images/3.jpeg', isFav: true },
+                { title: 'Whispers in the Dark', author: 'Emily Bronte', img: 'assets/images/4.jpeg', isFav: false },
+                { title: 'Fading Shadows', author: 'Michael Scott', img: 'assets/images/5.jpeg', isFav: false },
+                { title: 'Broken Dreams', author: 'Sarah Williams', img: 'assets/images/6.jpeg', isFav: false },
+                { title: 'Journey to Nowhere', author: 'Tom Jenkins', img: 'assets/images/7.jpeg', isFav: true },
+                { title: 'Chasing the Wind', author: 'Alice Walker', img: 'assets/images/8.jpeg', isFav: true },
+                { title: 'Beyond the Horizon', author: 'David Brown', img: 'assets/images/9.jpeg', isFav: false },
+                { title: 'Fragments of Time', author: 'Laura Hill', img: 'assets/images/10.jpeg', isFav: true }
             ],
             title: "Shereds for Sell",
             description: "Shereds for Sell.............",
@@ -42,6 +42,9 @@ const app = Vue.createApp({
         handleMouseMove(e){
             this.x =  e.offsetX,
             this.y = e.offsetY
+        },
+        toogleFav(book){
+            book.isFav = !book.isFav
         }
 
     },
