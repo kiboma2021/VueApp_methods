0071dfb725d96d9f6b9347f91d44111555db7cd6
kiboma <omayioben@gmail.com>
2024-09-06 08:34:17 +0300

    Return filtered books

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ const app = Vue.createApp({
         }
 
     },
+    computed: {
+        filteredBooks(){
+            return this.books.filter(book => book.isFav)
+        }
+    }
 });
 
 app.mount('#app')
